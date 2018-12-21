5d1b221f57b3df6ae2316ec5c561402c18c3027c
tinabayi <bayizerechristine4@gmail.com>
2018-12-21 15:55:19 +0200

    Istyle web page

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,23 @@
-$(document).ready(function(){
-    $("#question form").submit(function(event) {
-     
-        var answer1Input = $("input:radio[name=question1]:checked").val();
-        var answer2Input= $("input:radio[name=question2]:checked").val() ; 
-         var answer3Input = $("input:radio[name=question3]:checked").val();
-         var answer4Input = $("input:radio[name=question4]:checked").val();
-         
-          // console.log(answer1Input, answer2Input,answer3Input,answer4Input);
-          // event.preventDefault();
-          var scrore=0;
-          if(answer1Input===true){
-          score+=10;
-          }else if(answer2Input===true){
-              scrore+=10;
-          }else if(answer3Input===true){
-              score+=10;
-          }else if(answer4Input===true){
-              score+=10;
-          }else{
-              score-=10;
-          }
-          
-    });
+$(document).ready(function() {
+  $("#question form").submit(function(event) {
 
+ var questions=["question1","question2","question2","question4"];
+    
+    var score = 0;
+    questions.forEach(function(question){
+        var userInput=$("input:radio[name=" + question + "]:checked").val();
+    if (userInput === "right") {
+      score += 10;
+    }
+    
+    });
+    
+    
+    
+    
+    $(".mark").text(score);
+    $("#points").show();
+    event.preventDefault();
+});
+
+});
