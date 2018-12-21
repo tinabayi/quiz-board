3aedbf9c329a9aede534966b6e7f048ff6307bc6
tinabayi <bayizerechristine4@gmail.com>
2018-12-21 12:43:20 +0200

    I write code on html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-    $("#question form").submit(function() {
+    $("#question form").submit(function(event) {
      
         var answer1Input = $("input:radio[name=question1]:checked").val();
         var answer2Input= $("input:radio[name=question2]:checked").val() ; 
          var answer3Input = $("input:radio[name=question3]:checked").val();
          var answer4Input = $("input:radio[name=question4]:checked").val();
-         alert(answer1Input, answer2Input, answer3Input, answer4Input)
+         
            console.log(answer1Input, answer2Input,answer3Input,answer4Input);
+           event.preventDefault();
     });
 
 });
