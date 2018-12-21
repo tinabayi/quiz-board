a6a8e60408de9dea22c476ee4eaf9a5a7dd7c090
tinabayi <bayizerechristine4@gmail.com>
2018-12-21 13:16:47 +0200

    I add a div in html file

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,20 @@ $(document).ready(function(){
          var answer3Input = $("input:radio[name=question3]:checked").val();
          var answer4Input = $("input:radio[name=question4]:checked").val();
          
-           console.log(answer1Input, answer2Input,answer3Input,answer4Input);
-           event.preventDefault();
+          // console.log(answer1Input, answer2Input,answer3Input,answer4Input);
+          // event.preventDefault();
+          var scrore=0;
+          if(answer1Input===true){
+          score+=10;
+          }else if(answer2Input===true){
+              scrore+=10;
+          }else if(answer3Input===true){
+              score+=10;
+          }else if(answer4Input===true){
+              score+=10;
+          }else{
+              score-=10;
+          }
+          
     });
 
-});
